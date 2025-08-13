2bc59ee9d769a1603b7086df5da25c27c8d92203
xv13r <44001932+xv13r@users.noreply.github.com>
2025-08-13 16:54:24 -0300

    added spanish sound

--- capgensounds/main.go
+++ capgensounds/main.go
@@ -18,7 +18,7 @@ import (
 
 const headerLen = 44
 
-var langs = []string{"en", "ru", "zh", "ja", "pt"}
+var langs = []string{"en", "es", "ru", "zh", "ja", "pt"}
 
 func writeVar(w io.Writer, b []byte, prefix string) {
 	i := 0
